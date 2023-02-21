652d09fe74a0234d42171a2ee299fb881b19c00b
Mikhail Khalilov <miharulidze@gmail.com>
2023-02-21 12:24:33 +0100

    add l2 experiments

---
+++ examples/synthetic/handlers/synthetic.l2.c
@@ -0,0 +1,48 @@
+// Copyright 2022 ETH Zurich
+//
+// Licensed under the Apache License, Version 2.0 (the "License");
+// you may not use this file except in compliance with the License.
+// You may obtain a copy of the License at
+//
+//     http://www.apache.org/licenses/LICENSE-2.0
+
+//
+// Unless required by applicable law or agreed to in writing, software
+// distributed under the License is distributed on an "AS IS" BASIS,
+// WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+// See the License for the specific language governing permissions and
+// limitations under the License.
+
+#ifndef HOST
+#include <handler.h>
+#include <packets.h>
+#include <spin_dma.h>
+#else
+#include <handler_profiler.h>
+#endif
+
+#include "synthetic.h"
+
+__handler__ void synthetic_ph(handler_args_t *args)
+{
+    task_t *task = args->task;
+    benchmark_params_t params = *(benchmark_params_t *)task->handler_mem;
+    char *ptr = (char *)task->handler_mem + sizeof(benchmark_params_t);
+    spin_dma_t dma;
+    ip_hdr_t *ip_hdr = (ip_hdr_t *)task->pkt_mem;
+    uint8_t *payload_addr = (uint8_t *)ip_hdr;
+    uint32_t payload_len = ip_hdr->length;
+
+    for (int i = 0; i < params.loop_spin_count; i++) {
+        spin_dma((void *)ptr, payload_addr, payload_len, 0, 0, &dma);
+        spin_dma_wait(dma);
+    }
+}
+
+void init_handlers(handler_fn * hh, handler_fn *ph, handler_fn *th, void **handler_mem_ptr)
+{
+    volatile handler_fn handlers[] = {NULL, synthetic_ph, NULL};
+    *hh = handlers[0];
+    *ph = handlers[1];
+    *th = handlers[2];
+}
